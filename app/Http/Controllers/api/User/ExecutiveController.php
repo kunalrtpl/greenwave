542bdc1b9a88590ece019f8e3a198c8241f72152
kunal_royalways <kunal@rtpltech.com>
2025-12-10 15:44:13 +0530

    Updates

--- app/Http/Controllers/api/User/ExecutiveController.php
+++ app/Http/Controllers/api/User/ExecutiveController.php
@@ -2239,7 +2239,13 @@ class ExecutiveController extends Controller
             $userids = \DB::table('user_departments')->where('department_id',2)->pluck('user_id')->toArray();
             //$userIds = \App\UserDepartmentRegion::wherein('sub_region_id',$reporting_resp['sub_region_ids'])->pluck('user_id')->toArray();
            // $users  = \DB::table('users')->wherein('id',$userIds)->get();
-            $users  = \DB::table('users')->whereNotin('id',[16,17])->wherein('id',$userids)->where('status',1)->get();
+            $users = \DB::table('users')
+                ->select('id', 'name', 'mobile', 'designation', 'email', 'image')
+                ->whereNotIn('id', [16, 17])
+                ->whereIn('id', $userids)
+                ->where('status', 1)
+                ->get();
+
             $dealers =  \DB::table('dealers')->select('id','business_name','owner_name')->whereNotin('id',[1,5,7])->whereNULL('parent_id')/*->wherein('city',$reporting_resp['cities'])*/->where('status',1)->get();
             $message = "Fetched successfully";
             $result['users'] =  $users;
