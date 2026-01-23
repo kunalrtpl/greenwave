e9354bc1ac73d30009c0ec66c5782b07807824d0
kunal_royalways <kunal@rtpltech.com>
2026-01-23 15:51:41 +0530

    Updates

--- app/Http/Controllers/api/User/ExecutiveController.php
+++ app/Http/Controllers/api/User/ExecutiveController.php
@@ -2287,7 +2287,7 @@ class ExecutiveController extends Controller
                 ->where('status', 1)
                 ->get();
 
-            $dealers =  \DB::table('dealers')->select('id','business_name','owner_name')->whereNotin('id',[1,5,7])->whereNULL('parent_id')/*->wherein('city',$reporting_resp['cities'])*/->where('status',1)->get();
+            $dealers =  \DB::table('dealers')->select('id','business_name','owner_name','address','city','city','office_phone','owner_mobile')->whereNotin('id',[1,5,7])->whereNULL('parent_id')/*->wherein('city',$reporting_resp['cities'])*/->where('status',1)->get();
             $message = "Fetched successfully";
             $result['users'] =  $users;
             $result['dealers'] =  $dealers;
